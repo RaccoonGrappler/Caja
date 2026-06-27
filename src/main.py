2bed2de57f2ff6366501be0779a533b77c8d021f
Carlos Pizarro <pizarro.c.barboza@outlook.com>
2026-06-27 00:44:11 -0600

    Se corrigio el ingles y se agregaron opciones de mejora de estadisticas.

--- src/main.py
+++ src/main.py
@@ -4,18 +4,34 @@ def main():
     print("Bienvenido cazador! Es bueno verte por aca!")
     print("Que cazaremos el dia de hoy?")
 
-    monsters = ["Zinogre", "Rathalos", "Brachydios", "Kirin", "Mizutsune", "Rajang", "Deviljho"]
+    monsters = ["Rathalos", "Kirin", "Mizutsune", "Rajang", "Zoh Shia", "Nu Udra", "Chameleos", "Valstrax"]
 
     print(f"The options are: {', '.join(monsters)}")
 
-    monsterToHunt = None
+    monsterToHunt = input("Cual monstruo cazaremos?: ")
     while monsterToHunt not in monsters:
-        monsterToHunt = input("Enter your choice: ")
+        monsterToHunt = input("Cual monstruo cazaremos?: ")
 
         if monsterToHunt not in monsters:
             print("Lo lamentamos cazador! Ese monstruo no esta disponible en este momento. Porfavor elige uno de la lista.")
 
     print(f"Vas a cazar un {monsterToHunt} miauster? You need to eat good firrrrst!")
+
+    platillosDisponibles = {
+        "Ataque": "Carne de Wyvern", 
+        "Resistencia": "Patas de pescado", 
+        "Defensa": "Hongo gigante de wyveria", 
+        "Balanceado": "Puggie a la braza muajaja!"
+    }
+
+    print("Hola miauestro! Mira lo que tenemos en el menu miau!: ")
+    for platillo in platillosDisponibles.keys():
+        print(f"{platillo}: {platillosDisponibles[platillo]}")
+
+    opcionPlatillo = input("Que estadistica quieres mejorar?: ")
+    while opcionPlatillo not in platillosDisponibles:
+        opcionPlatillo = input("Que estadistica quieres mejorar?: ")
+
     print("Cocina dandolo todo fuertemente!....")
     print("Cazador lleno y palico relleno, estamos listos para la caza!!!")
     print("*Sonido de iniciacion de la caza*!!!! Paraaaaaaa!!!")
